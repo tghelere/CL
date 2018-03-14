4b98028a6c2ee703e778d9c37463774064e4418d
T Ghelere <ghelere@outlook.com>
2018-03-14 16:40:15 -0300

    Models and migration -Create models (banners, users, services, solutions) -Create migrations (banners, users, services, solutions) -Improve folder structure controllers

--- database/migrations/2018_03_14_024838_create_banners_table.php
+++ database/migrations/2018_03_14_024838_create_banners_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateBannersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('banners', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('image', 100)->nullable();
-            $table->rememberToken();
+            $table->string('page');
+            $table->boolean('status')->default(true);
+            $table->string('image', 100);
+            $table->string('title')->nullable();
+            $table->string('description')->nullable();
+            $table->string('link')->nullable();
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('banners');
     }
 }
--- database/migrations/2018_03_14_025103_create_services_table.php
+++ database/migrations/2018_03_14_025103_create_services_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateServicesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('services', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('image', 100)->nullable();
-            $table->rememberToken();
+            $table->string('slug');
+            $table->string('title');
+            $table->text('description');
+            $table->string('icon');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('services');
     }
 }
